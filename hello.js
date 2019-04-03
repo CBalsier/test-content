8745f93293980de8d007558a4fdb9223bafd32c0
CBalsier <cecile.balsier@institutoptique.fr>
2019-04-03 16:48:48 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,9 +5,27 @@ const projectCountP = fetch('https://solidata-api.co-demos.com/api/dsi/infos/get
   .then(r => r.data.data_raw.f_data_count)
 
 document.addEventListener('DOMContentLoaded', e => {
+  
+  const DELAY = 1000;
   const counter = document.querySelector('.counter')
   projectCountP.then(projectCount => {
-    //const start = performance.now();
+    const start = performance.now();
+    
+    (function step(){
+      requestAnimationFrame(now => {
+        const elapsed = now - start;
+        let fraction = elapsed/DELAY;
+        if(fraction >=1)
+          fraction = 1;
+        const toDisplay = projectCount * (1- (fraction === 1 ? 0 : Math.pow(2, -10*fraction)));
+        
+        counter.textContent = Math.round(Math.max(0, toDisplax));
+        
+        if(fraction <1){
+          step();
+        }
+      })
+    })();
     console.log(projectCount)
   })
 
