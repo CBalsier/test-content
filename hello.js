9e01ff4aef7d3dcc7a092428bd939e1e29c36f83
CBalsier <cecile.balsier@institutoptique.fr>
2019-04-03 16:38:29 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
 console.log("hello world from script"); 
+
+const projectCountP = fetch('https://solidata-api.co-demos.com/api/dsi/infos/get_one/5c7f0438328ed72e431f338e')
+  .then(r => r.json())
+  .then(r => r.data.data_raw.f_data_count)
+
+console.log(projectCountP)
